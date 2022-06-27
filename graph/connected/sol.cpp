3fb1e3c51d203408873f1912a4002167ff34f266
ngqp2k <ngqp2k@outlook.com>
2022-06-27 15:17:11 +0700

    add

---
+++ graph/connected/sol.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
+
+// num[]: thu tu duyet DFS cua moi dinh
+// low[]: 
+// tail[]: thoi diem ket thuc khi duyet xong dinh do
+
+
+const int mxN = 100;
+
+int n, m;
+vector<int> g[mxN];
+bool vis[mxN];
+int num[mxN], low[mxN], tail[mxN];
+int timeDfs = 0;
+
+void dfs(int u) {
+    ;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; ++i) {
+        int u, v; cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        if (!num[i]) {
+            dfs(i);
+        }
+    }
+
+    return 0;
+}
