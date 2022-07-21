3d0c9cdd9bbafa6109232d83681c0ad8ecc7b549
ngqp2k <ngqp2k@outlook.com>
2022-07-21 12:33:28 +0700

    'add'

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 void __print(int x) {cerr << x;}
--- vnoi/liq/sol.cpp
+++ vnoi/liq/sol.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 void __print(int x) {cerr << x;}
@@ -26,12 +25,28 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 
 #define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
 
+const int mxn = 1e3 + 11;
+int n;
+int a[mxn], dp[mxn];
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n; cin >> n;
-    cout << n + 10 << "\n";
+		cin >> n;
+		for (int i = 1; i <= n; ++i)
+			cin >> a[i];
+
+		int ans = 0;
+		for (int i = 1; i <= n; ++i) {
+			dp[i] = 1;
+			for (int j = 1; j < i; ++j)
+				if (a[j] < a[i])
+					dp[i] = max(dp[i], dp[j] + 1);
+				ans = max(ans, dp[i]);
+		}
+
+		cout << ans << "\n";
 
     return 0;
 }
